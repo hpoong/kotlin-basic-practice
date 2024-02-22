9a220a8f8f981c7c39f4a7d25025eb9dadcd5f36
mason <mason@carrot-i.com>
2024-02-22 18:54:16 +0900

    # 수정사항 - random ~ 조건문 학습

--- src/main/kotlin/com/kotlinproject/start/Main.kt
+++ src/main/kotlin/com/kotlinproject/start/Main.kt
@@ -1,6 +1,76 @@
 package com.kotlinproject.start
 
+import java.util.*
+import kotlin.random.Random
+
 fun main() {
-    print("Hello World");
+
+
+    // ******* random
+    val randomNumber = Random.nextInt(0, 100); // 0 ~ 99
+    println(randomNumber)
+
+    val randomNumber1 = Random.nextDouble(0.0, 1.0)
+    println(randomNumber1)
+
+
+    // *******  키보드 입력
+    val reader = Scanner(System.`in`)
+//    var test1 = reader.nextInt()
+//    var test2 = reader.next()
+//
+//    println(test1)
+//    println(test2)
+
+
+
+    // *******  조건문
+    var i = 6
+    if(i > 10)
+        println("10 보다 크다")
+    else if(i > 5)
+        println("5 보다 크다")
+    else
+        println("else")
+
+
+    // if문 대신 when 사용
+    when {
+        i > 10 ->
+            println("10 보다 크다")
+        i > 5 ->
+            println("5 보다 크다")
+        else ->
+            println("else")
+    }
+
+    var result1 = when {
+        i > 10 ->
+            "10 보다 크다"
+        i > 5 ->
+            "5 보다 크다"
+        else ->
+            "else"
+    }
+
+
+    var result2 = if(i > 10)
+        "10 보다 크다"
+    else if(i > 5)
+        "5 보다 크다"
+    else
+        "else"
+
+    println("result1 : ${result1}")
+    println("result2 : ${result2}")
+
+
+
+    // JAVA : boolean result = i > 10 ? true : false;
+    var it = 5;
+    val result55 = if(it > 10) true else false
+    println(result55)
+
+
 }
 
